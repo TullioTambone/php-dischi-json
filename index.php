9e8954c4d6550440cbd16afc4a5ddca90344ea28
TullioTambone <tulliotambone97@gmail.com>
2023-05-23 13:35:13 +0200

    card statiche completate

--- index.php
+++ index.php
@@ -1,21 +1,43 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-        <div id="app">
-            
-        </div>
+    <div id="app">
+        <header>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="" alt="">logo
+                    </a>
+                </div>
+            </nav>
+        </header>
+
+        <body>
+            <div class="container my-4">
+                <div class="row gap-2 m-auto justify-content-center">
+                    <div class="card col-12 col-md-5 col-lg-3 text-center text-white p-3" v-for="(element, index) in data" :key="index">
+                        <img :src="`${element.poster}`" alt="">
+                        <h5 class="m-0 mt-2">{{element.title}}</h5>
+                        <span>{{element.author}}</span>
+                        <h6>{{element.year}}</h6>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </div>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-        <script src="/main.js"></script>
-    </body>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
+</body>
 </html>
--- main.js
+++ main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      apiUrl: 'server.php',
+      data: ''
+    }
+  },
+  mounted(){
+    this.chiamataApi()
+  },
+  methods:{
+    chiamataApi(){
+      axios.get(this.apiUrl)
+      .then((res) => {
+        this.data = res.data
+      });
     }
   }
+
 }).mount('#app')
